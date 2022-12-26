101603116cade21388b2b6b885549ef3804318a8
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-12-26 21:35:58 +0800

    12/26/22

---
+++ components/Meta.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+function Meta() {
+  return (
+    <Fragment>
+      <meta name="title" content="Carl Caraan Web Developer"></meta>
+      <meta
+        name="description"
+        content="Carl Caraan - Web Developer based on Laguna, Philippines"
+      ></meta>
+      <meta
+        name="keywords"
+        content="CarlCaraan, Carl Caraan, Web Developer, Laguna, Santa Cruz, Philippines"
+      ></meta>
+      <meta name="robots" content="index, follow"></meta>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+      <meta name="language" content="English"></meta>
+    </Fragment>
+  );
+}
+
+export default Meta;
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Meta from "../components/Meta";
 
 export default function Document() {
   return (
     <Html lang="en" className="">
-      <Head />
+      <Head>
+        <Meta />
+      </Head>
       <body className="antialiased scroll-smooth">
         <Main />
         <NextScript />
