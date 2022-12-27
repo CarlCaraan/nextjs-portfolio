3d41eb2cabd570fd8cf79f5b0c2a6d9fedf85e38
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-12-27 15:50:58 +0800

    12/27/22

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,76 @@ export default function Home() {
     <Fragment>
       <title>Carl Caraan | Web Developer</title>
       <Navbar />
+      <main>
+        {/* ========= Start Hero Section ========= */}
+        <section
+          id="hero"
+          className="container mx-auto px-6 
+        md:px-24"
+        >
+          {/* Start Flex Container */}
+          <div
+            className="flex flex-col-reverse items-center
+            lg:flex-row"
+          >
+            {/* Left Section */}
+            <div className="lg:w-1/2">
+              <h1
+                className="text-2xl font-sansB text-center mt-12
+                lg:text-start sm:text-5xl lg:mt-12 xl:mt-0"
+              >
+                Let&#39;s Build <br /> Something Epic
+              </h1>
+              <div
+                className="mt-6 text-grayLight text-center 
+                lg:text-start"
+              >
+                <span className="sm:text-2xl font-sansSB">Web Developer</span>{" "}
+                <span className="sm:text-2xl">
+                  based in Laguna, Philippines
+                </span>
+              </div>
+
+              <div
+                className="mt-6 
+                sm:mt-12 xl:mt-16 2xl:mt-40"
+              >
+                <Link
+                  href="/about"
+                  className="text-black block text-center font-sansSB underline hover:no-underline
+                  lg:text-start"
+                >
+                  Discover more about me
+                </Link>
+              </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="lg:w-1/2">
+              <Image
+                className="w-3/4 mx-auto 
+                md:w-5/6 lg:mr-0"
+                src="/../public/images/userbg.png"
+                alt="carlcaraan"
+                width={500}
+                height={500}
+              />
+            </div>
+          </div>
+          {/* End Flex Container */}
+        </section>
+        {/* ========= End Hero Section ========= */}
+
+        {/* ========= Start Content Section ========= */}
+        <section
+          className="mt-12 bg-panel 
+          xl:-mt-14"
+          id="content"
+        >
+          <h1 className="text-9xl mx-auto h-screen"></h1>
+        </section>
+        {/* ========= End Content Section ========= */}
+      </main>
     </Fragment>
   );
 }
