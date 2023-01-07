aaf9f61867163e1e8e4fee28bf97b24c7d38c48b
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-01-07 21:33:27 +0800

    1/7/23

--- components/about/SkillsSection.jsx
+++ components/about/SkillsSection.jsx
@@ -15,10 +15,10 @@ function SkillsSection({ props }) {
           <AboutHeading props={props.text} />
           {/* End About Heading */}
 
-          {/* Start Skills Content */}
+          {/* Start Skills Heading */}
           <div
             className="col-start-1 col-span-9 justify-self-stretch py-4
-                md:col-start-2 md:py-24"
+                md:col-start-2 md:py-12"
           >
             {/* Start Heading Text */}
             <div
@@ -33,10 +33,70 @@ function SkillsSection({ props }) {
             </div>
             {/* End Heading Text */}
           </div>
-          {/* End Skills Content */}
+          {/* End Skills Heading */}
         </div>
         {/* End Grid Container */}
       </div>
+
+      {/* Start Skills Content */}
+      <div className="bg-panel py-16">
+        <div className="section-container-fluid">
+          {/* Start Grid Container */}
+          <div className="grid">
+            <div
+              className="col-start-1 col-span-1 w-60 hidden 
+              md:block"
+            ></div>
+            <div
+              className="col-start-1 col-span-10 
+              md:col-start-2"
+            >
+              {/* Start Inner Grid Container */}
+              <div className="grid grid-cols-7 grid-rows-3 gap-6">
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:col-span-2"
+                >
+                  <h4 className="skills-heading">UX/UI</h4>
+                </div>
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:row-span-2 md:col-span-3"
+                >
+                  <h4 className="skills-heading">Web</h4>
+                </div>
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:col-span-2"
+                >
+                  <h4 className="skills-heading">CSS Frameworks</h4>
+                </div>
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:row-span-2 md:row-start-2 md:col-span-2"
+                >
+                  <h4 className="skills-heading">Technology Stacks</h4>
+                </div>
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:row-start-3 md:col-span-3"
+                >
+                  <h4 className="skills-heading">Database</h4>
+                </div>
+                <div
+                  className="custom-border-dashed col-span-7 
+                  md:row-span-1 md:col-span-2"
+                >
+                  <h4 className="skills-heading">Tools</h4>
+                </div>
+              </div>
+              {/* End Inner Grid Container */}
+            </div>
+          </div>
+          {/* End Grid Container */}
+        </div>
+      </div>
+      {/* End Skills Content */}
     </section>
   );
 }
