8976f210d0b839ae991a75550e6b665e2e151673
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-01-16 13:03:48 +0800

    1/16/23

--- components/about/SkillsSection.jsx
+++ components/about/SkillsSection.jsx
@@ -240,7 +240,7 @@ function SkillsSection({ props }) {
                 <Link
                   className="text-base font-sansSB underline hover:no-underline 
                   sm:text-xl"
-                  href="/downloads/caraan_resume.pdf"
+                  href="/downloads/caraan_resume_v2.pdf"
                   target="_blank"
                 >
                   Download CV
