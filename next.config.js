6e3879107faca92fcb85be9cd2e0f364fd1440c5
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-04-10 15:20:32 +0800

    4/10/23

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// Minified JS
-const withPlugins = require("next-compose-plugins");
-const TerserPlugin = require("terser-webpack-plugin");
-module.exports = withPlugins([
-  {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.optimization.minimizer = [
-          new TerserPlugin({
-            terserOptions: {
-              compress: {
-                drop_console: true,
-              },
-            },
-          }),
-        ];
-      }
-      return config;
-    },
-  },
-]);
-
 const nextConfig = {
   reactStrictMode: true,
 };
