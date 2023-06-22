2cabaa2ba23c9686de31b81760f8e99df568fdfe
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-06-22 17:28:01 +0800

    6/22/23

--- pages/experience.js
+++ pages/experience.js
@@ -123,7 +123,9 @@ function experience() {
 
                       {/* Item */}
                       <div>
-                        <h4 className="education-title">CEO & CO-Founder</h4>
+                        <h4 className="education-title">
+                          CO-Founder & Web Developer
+                        </h4>
                         <p className="education-subtitle">
                           Matrix Software Solutions PH
                         </p>
