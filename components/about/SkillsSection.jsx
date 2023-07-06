b61af5da079a4ea6a4b4ee484b6184034ce24cab
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-07-06 11:24:50 +0800

    7/6/23

--- components/about/SkillsSection.jsx
+++ components/about/SkillsSection.jsx
@@ -256,7 +256,7 @@ function SkillsSection({ props }) {
                   href="/downloads/Carl Caraan - Resume.pdf"
                   target="_blank"
                 >
-                  Download CV
+                  Download Resume
                 </Link>
               </div>
 
