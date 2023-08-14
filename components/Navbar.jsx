f341ede9a810ebac54e56458e9cfa33b13d375e6
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-08-14 14:04:42 +0800

    8/14/23

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { useTheme } from "next-themes";
 
 // Components
